78ea34270d4822979799da30abc5a2689861cbba
AJ Schulte <captrockstar007@gmail.com>
2025-09-20 15:55:52 -0400

    Hidden API KEY

--- deck_builder/lib/data/util/api.dart
+++ deck_builder/lib/data/util/api.dart
@@ -4,7 +4,7 @@ import 'dart:convert';
 import 'package:deck_builder/data/model/card.dart';
 
 class APIRunner {
-  final String apiKey = 'd8d05daadb1ee463df6dc55e4fa0b546bb5bf9c33351c98da1fe51d558c04b60';
+  final String apiKey = 'API KEY';
   final String baseURL = 'https://apitcg.com/api/union-arena/cards?';
 
   Future<List<Card>?> runAPI(String url) async {
